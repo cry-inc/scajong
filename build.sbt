88c0bd3ecdd2546d6344a43592466a5efb87fb01
cry-inc <cry-inc@users.noreply.github.com>
2012-11-02 13:33:19 +0100

    Added specs testing framework and first test

--- build.sbt
+++ build.sbt
@@ -21,3 +21,8 @@ scalaSource in Compile := file("src/scajong/")
 scalaSource in Test := file("test/scajong/")
 
 libraryDependencies <+= scalaVersion { "org.scala-lang" % "scala-swing" % _ }
+
+libraryDependencies ++= Seq("org.specs2" %% "specs2" % "1.12.2" % "test")
+ 
+resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+                  "releases"  at "http://oss.sonatype.org/content/repositories/releases")
---
+++ test/scajong/model/TileTypeSpec.scala
@@ -0,0 +1,26 @@
+package scajong.model
+
+import org.specs2.mutable._
+
+class TileTypeSpec extends Specification {
+	"A TileType" should {
+		val tileType = new TileType(23, "t42")
+	  
+	  "have a id" in {
+			tileType.id must be_==(23)
+		}
+		
+		"have a name" in {
+			tileType.name must be_==("t42")
+		}
+		
+		"be a string" in {
+		  tileType.toString must be_==("TileType[23,\"t42\"]")
+		}
+		
+		"load tile types from a file" in {
+		  val types = TileType.LoadTileTypes("tiles.txt")
+		  types must have size(47)
+		}
+	}
+}
