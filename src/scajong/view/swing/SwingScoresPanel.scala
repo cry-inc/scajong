323f9fadb3af7b2e5883f60a6a79e731e0156618
n0rf <andi_snoopy@web.de>
2012-11-30 15:23:14 +0100

    Refined the score tables in the swing view

--- src/scajong/view/swing/SwingScoresPanel.scala
+++ src/scajong/view/swing/SwingScoresPanel.scala
@@ -6,11 +6,11 @@ import swing.event._
 
 class AddScoreEvent(val setup:Setup, val name:String, val ms:Int) extends Event
 
-class SwingScoresPanel(val scores:Scores) extends GridPanel(1, 1) {
+class SwingScoresPanel(scores:Scores) extends GridPanel(1, 1) {
 
   var currentSetup:Setup = null
   var currentTime = 0
-  
+
   reactions += {
     case e:EditDone => {
       publish(new AddScoreEvent(currentSetup, e.source.text, currentTime))
@@ -18,62 +18,68 @@ class SwingScoresPanel(val scores:Scores) extends GridPanel(1, 1) {
       showScores(currentSetup)
     }
   }
-  
-  def prepareGrid(setup:String, scoreList:List[ScoreEntry]) {
+
+  def prepareGrid(setupName:String) {
     contents.clear
-    addCaptionsToGrid(setup)
+    addCaptionsToGrid(setupName)
   }
-  
-  def addCaptionsToGrid(setup:String) {
+
+  def addCaptionsToGrid(setupName:String) {
     contents += new Label("------")
-    contents += new Label(setup)
+    contents += new Label(setupName)
     contents += new Label("------")
     contents += new Label("Position")
     contents += new Label("Name")
     contents += new Label("Seconds")
   }
-  
+
   def addScore(setup:Setup, ms:Int) {
-    if (scores.isInScoreboard(setup, ms)) {
-	    visible = false
+    if (!scores.isInScoreboard(setup, ms)) {
+      showScores(setup)
+    } else {
+      visible = false
+      prepareGrid(setup.name)
+      val pos = scores.getScorePosition(setup, ms)
+      val textField = new TextField("Anonymous")
+      currentSetup = setup
+      currentTime = ms
+      listenTo(textField)
       val scoreList = scores.getScores(setup)
-	    rows = scoreList.length + 3
-	    columns = 3
-	    prepareGrid(setup.name, scoreList)
-	    val pos = scores.getScorePosition(setup, ms)
-	    val textField = new TextField("Anonymous")
-	    currentSetup = setup
-	    currentTime = ms
-	    textField.selectAll
-	    listenTo(textField)
-		  for (i <- 0 until pos) {
-		    contents += new Label((i+1).toString)
-	    	contents += new Label(scoreList(i).name)
-		    contents += new Label((scoreList(i).ms / 1000.0).toString)
-		  }
-	    contents += new Label((pos+1).toString)
-	    contents += textField
-	    contents += new Label((ms / 1000.0).toString)
-	    for (i <- pos until scoreList.length-1) {
-		    contents += new Label((i+2).toString)
-	    	contents += new Label(scoreList(i).name)
-		    contents += new Label((scoreList(i).ms / 1000.0).toString)
-		  }
-	    visible = true
-    } else showScores(setup)
+      rows = Scores.PerSetupEntries + 2
+      columns = 3
+      for (i <- 0 until Scores.PerSetupEntries) {
+        contents += new Label((i+1).toString)
+        if (i < pos) {
+          contents += new Label(scoreList(i).name)
+          contents += new Label((scoreList(i).ms / 1000.0).toString)
+        } else if (i == pos) {
+          contents += textField
+          contents += new Label((ms / 1000.0).toString)
+        } else {
+          contents += new Label(scoreList(i-1).name)
+          contents += new Label((scoreList(i-1).ms / 1000.0).toString)
+        }
+      }
+      visible = true
+    }
   }
-  
+
   def showScores(setup:Setup) {
     visible = false
     val scoreList = scores.getScores(setup)
-    rows = scoreList.length + 2
+    rows = Scores.PerSetupEntries + 2
     columns = 3
-    prepareGrid(setup.name, scoreList)
-	  for (i <- 0 until scoreList.length) {
-	    contents += new Label((i+1).toString)
-	    contents += new Label(scoreList(i).name)
-	    contents += new Label((scoreList(i).ms / 1000.0).toString)
-	  }
+    prepareGrid(setup.name)
+    for (i <- 0 until Scores.PerSetupEntries) {
+      contents += new Label((i+1).toString)
+      if (i<scoreList.length) {
+        contents += new Label(scoreList(i).name)
+        contents += new Label((scoreList(i).ms / 1000.0).toString)
+      } else {
+        contents += new Label()
+        contents += new Label()
+      }
+    }
     visible = true
   }
 }
